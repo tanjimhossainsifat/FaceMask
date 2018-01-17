66e89727e1328ba2982b21aa3887602e34c1bd74
Tanjim Hossain <tanjim@revesoft.com>
2018-01-18 02:11:03 +0600

    Single face is cropped from image

--- FaceMask/FaceMask/FaceMaskViewController.m
+++ FaceMask/FaceMask/FaceMaskViewController.m
@@ -20,20 +20,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIImage *originalImage = [UIImage imageNamed:@"test2"];
+    UIImage *originalImage = [UIImage imageNamed:@"test3"];
     CGFloat originalRatio = originalImage.size.height/originalImage.size.width;
     
     imageView = [[UIImageView alloc] initWithImage:originalImage];
-    [imageView setFrame:CGRectMake(0, 0, self.view.frame.size.width, originalRatio * self.view.frame.size.width)];
-    [imageView setCenter:CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2)];
+    [imageView setFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width,  originalRatio * [[UIScreen mainScreen] bounds].size.width)];
     
     [self.view addSubview:imageView];
     
+    [self detectFaceFeatures];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) detectFaceFeatures {
+    
+    CIImage *originalImage = [CIImage imageWithCGImage:imageView.image.CGImage];
+    
+    CIDetector *faceDetector = [CIDetector detectorOfType:CIDetectorTypeFace context:nil options:[NSDictionary dictionaryWithObject:CIDetectorAccuracyLow forKey:CIDetectorAccuracy]];
+    
+    NSArray *features = [faceDetector featuresInImage:originalImage];
+    
+    for(CIFaceFeature *face in features) {
+        
+        if(face.hasLeftEyePosition && face.hasRightEyePosition && face.hasMouthPosition) {
+            
+            
+            CGRect modifiedRect = face.bounds;
+            CGRect originalRect = originalImage.extent;
+            modifiedRect.size.width = modifiedRect.size.width * (imageView.frame.size.width/originalRect.size.width);
+            modifiedRect.size.height = modifiedRect.size.height * (imageView.frame.size.height/originalRect.size.height) ;
+            modifiedRect.origin.x = modifiedRect.origin.x * (imageView.frame.size.width/originalRect.size.width);
+            modifiedRect.origin.y = modifiedRect.origin.y * (imageView.frame.size.height/originalRect.size.height);
+            modifiedRect.origin.y = imageView.frame.size.height - (modifiedRect.origin.y+modifiedRect.size.height);
+            
+            
+            CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+            maskLayer.path = CGPathCreateWithRect(modifiedRect, nil);
+            imageView.layer.mask = maskLayer;
+        }
+    }
 }
-
 @end
